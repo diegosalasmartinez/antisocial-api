69b243c3795efcec078f805838f6a2780a8c7561
Diego Salas <diesalasmart@gmail.com>
2022-03-18 16:14:47 -0500

    select -posts on get categories

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -1,7 +1,7 @@
 const Category = require('../models/CategoryModel')
 
 const getCategories = async (req, res) => {
-    const categories = await Category.find().sort('name');
+    const categories = await Category.find().sort('name').select('-posts');
     res.status(200).json(categories);
 }
 
