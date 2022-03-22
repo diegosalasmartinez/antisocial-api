eef15ca68834b0f15335b2d94a03301f792ed58d
Diego Salas <diesalasmart@gmail.com>
2022-03-21 22:37:14 -0500

    migrate populte function for aggregate pipeline in post controller

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,26 +1,96 @@
 const Post = require('../models/PostModel')
 const Category = require('../models/CategoryModel')
 const User = require('../models/UserModel')
+const mongoose = require('mongoose')
+
+const projectOptionsPost = { 
+    'author.password': 0, 
+    'author.posts': 0, 
+    'author.likes': 0, 
+    'author.dislikes': 0, 
+    'author.saves': 0, 
+    'author.followers': 0, 
+    'author.following': 0, 
+    'category.posts': 0,
+    'replies': 0,
+}
+
+const projectOptionsSavedPost = { 
+    'posts.author.password': 0, 
+    'posts.author.posts': 0, 
+    'posts.author.likes': 0, 
+    'posts.author.dislikes': 0, 
+    'posts.author.saves': 0, 
+    'posts.author.followers': 0, 
+    'posts.author.following': 0, 
+    'posts.category.posts': 0,
+    'posts.replies': 0,
+}
 
 const getPostsByFollowingUsers = async (req, res) => {
     const user = req.user;
 
     const users = await User.findById(user._id).select('following');    
-    const usersId = [...users.following, user._id];
-    const posts = await Post.find({ author: { $in: usersId } }).populate({ 
-        path: 'author',
-        select: '-password -posts -likes -dislikes -saves -followers -following'
-    }).populate({ path: 'category', select: '-posts' }).sort({date: -1});
+    const usersId = [...users.following, mongoose.Types.ObjectId(user._id)];
+
+    const posts = await Post.aggregate([
+        { $match: { 'author': { $in: usersId } } },
+        { $lookup: {
+            from: 'users', 
+            localField: 'author', 
+            foreignField: '_id', 
+            as: 'author' 
+        }},
+        { $unwind: '$author' },
+        { $lookup: { 
+            from: 'categories', 
+            localField: 'category', 
+            foreignField: '_id', 
+            as: 'category' 
+        }},
+        { $unwind: '$category' },
+        { $addFields: {
+            'numReplies': { $cond: { if: { $isArray: '$replies' }, then: { $size: '$replies' }, else: 0} },
+            'author.postNumber': { $cond: { if: { $isArray: '$author.posts' }, then: { $size: '$author.posts' }, else: 0} },
+            'author.followersNumber': { $cond: { if: { $isArray: '$author.followers' }, then: { $size: '$author.followers' }, else: 0} },
+            'author.followingNumber': { $cond: { if: { $isArray: '$author.following' }, then: { $size: '$author.following' }, else: 0} },
+        }},
+        { $project: projectOptionsPost },
+        { $sort: { 'date': -1 } }
+    ]) 
+
     res.status(200).json(posts);
 }
 
 const getPostsByCategory = async (req, res) => {
     const { categoryId } = req.params;
-    
-    const posts = await Post.find({ category: categoryId}).populate({ 
-        path: 'author',
-        select: '-password -posts -likes -dislikes -saves -followers -following'
-    }).populate({ path: 'category', select: '-posts' }).sort({ date: -1 });
+
+    const posts = await Post.aggregate([
+        { $match: { $expr: { $eq: ['$category', { $toObjectId: categoryId } ] } } },
+        { $lookup: {
+            from: 'users', 
+            localField: 'author', 
+            foreignField: '_id', 
+            as: 'author' 
+        }},
+        { $unwind: '$author' },
+        { $lookup: { 
+            from: 'categories', 
+            localField: 'category', 
+            foreignField: '_id', 
+            as: 'category' 
+        }},
+        { $unwind: '$category' },
+        { $addFields: {
+            'numReplies': { $cond: { if: { $isArray: '$replies' }, then: { $size: '$replies' }, else: 0} },
+            'author.postNumber': { $cond: { if: { $isArray: '$author.posts' }, then: { $size: '$author.posts' }, else: 0} },
+            'author.followersNumber': { $cond: { if: { $isArray: '$author.followers' }, then: { $size: '$author.followers' }, else: 0} },
+            'author.followingNumber': { $cond: { if: { $isArray: '$author.following' }, then: { $size: '$author.following' }, else: 0} },
+        }},
+        { $project: projectOptionsPost },
+        { $sort: { 'date': -1 } }
+    ])
+
     res.status(200).json(posts);
 }
 
@@ -37,82 +107,84 @@ const getMostLikedPosts = async (req, res) => {
         dateFrom.setDate(dateFrom.getDate() - 30);
     }
 
-    const projectionOptions = { 'author.password': 0, 'author.posts': 0, 'author.likes': 0, 'author.dislikes': 0, 'author.saves': 0, 'author.followers': 0, 'author.following': 0, 'category.posts': 0 }
-
     const posts = await Post.aggregate([
-        {
-            $match: { 'date': { $gte: dateFrom, $lte: dateTo } }
-        },
-        {
-            $lookup: {
-                from: 'users',
-                localField: 'author',
-                foreignField: '_id',
-                as: 'author'
-            }
-        },
-        {
-            $unwind: '$author'
-        },
-        {
-            $lookup: {
-                from: 'categories',
-                localField: 'category',
-                foreignField: '_id',
-                as: 'category'
-            }
-        },
-        {
-            $unwind: '$category'
-        },
-        {
-            $addFields: {
-                'numLikes': { $cond: { if: { $isArray: '$likes' }, then: { $size: '$likes' }, else: 0} },
-                'numDislikes': { $cond: { if: { $isArray: '$dislikes' }, then: { $size: '$dislikes' }, else: 0} },
-            }
-        },
-        {
-            $addFields: {
-                'reputation': { $cond: { if: { $lte: ['$numDislikes', { '$multiply': [0.5, '$numLikes'] } ]}, 
-                    then: { $subtract: ['$numLikes', '$numDislikes']}, 
-                    else: { $subtract: ['$numLikes', { '$multiply': [1.5, '$numDislikes'] } ]}
-                }}
-            }
-        },
-        {
-            $project: projectionOptions
-        },
-        {
-            $sort: { 'reputation': -1 }
-        },
-        {
-            $limit: 5
-        }
+        { $match: { 'date': { $gte: dateFrom, $lte: dateTo } } },
+        { $lookup: {
+            from: 'users',
+            localField: 'author',
+            foreignField: '_id',
+            as: 'author'
+        }}, 
+        { $unwind: '$author' },
+        { $lookup: {
+            from: 'categories',
+            localField: 'category',
+            foreignField: '_id',
+            as: 'category'
+        }},
+        { $unwind: '$category' },
+        { $addFields: {
+            'author.postNumber': { $cond: { if: { $isArray: '$author.posts' }, then: { $size: '$author.posts' }, else: 0} },
+            'author.followersNumber': { $cond: { if: { $isArray: '$author.followers' }, then: { $size: '$author.followers' }, else: 0} },
+            'author.followingNumber': { $cond: { if: { $isArray: '$author.following' }, then: { $size: '$author.following' }, else: 0} },
+            'numLikes': { $cond: { if: { $isArray: '$likes' }, then: { $size: '$likes' }, else: 0} },
+            'numDislikes': { $cond: { if: { $isArray: '$dislikes' }, then: { $size: '$dislikes' }, else: 0} },
+        }},
+        { $addFields: {
+            'reputation': { $cond: { if: { $lte: ['$numDislikes', { '$multiply': [0.5, '$numLikes'] } ]}, 
+                then: { $subtract: ['$numLikes', '$numDislikes']}, 
+                else: { $subtract: ['$numLikes', { '$multiply': [1.5, '$numDislikes'] } ]}
+            }}
+        }},
+        { $project: projectOptionsPost },
+        { $sort: { 'reputation': -1 } },
+        { $limit: 5 }
     ])
+
     res.status(200).json(posts);
 }
 
 const getSavedPosts = async (req, res) => {    
     const user = req.user;
-    const populateSavedPosts = { 
-        path: 'saves',
-        populate: [
-            {
-                path: 'author',
-                select: '-password -posts -likes -dislikes -saves -followers -following'
-            },
-            {
-                path: 'category',
-                select: '-posts'
-            },
-        ],
-        options: { sort: {date: -1} }
-    }
 
-    const users = await User.find({_id: user._id}).populate(populateSavedPosts).sort({date: -1}).select('-password');
-    const posts = users[0].saves;
+    const posts = await User.aggregate([
+        { $match: { $expr: { $eq: ['$_id', { $toObjectId: user._id } ] } } },
+        { $lookup: {
+            from: 'posts', 
+            localField: 'saves', 
+            foreignField: '_id', 
+            as: 'posts' 
+        }},
+        { $unwind: '$posts' },
+        { $lookup: {
+            from: 'users',
+            localField: 'posts.author',
+            foreignField: '_id',
+            as: 'posts.author'
+        }}, 
+        { $unwind: '$posts.author' },
+        { $lookup: {
+            from: 'categories',
+            localField: 'posts.category',
+            foreignField: '_id',
+            as: 'posts.category'
+        }},
+        { $unwind: '$posts.category' },
+        { $addFields: {
+            'numReplies': { $cond: { if: { $isArray: '$replies' }, then: { $size: '$replies' }, else: 0} },
+            'author.postNumber': { $cond: { if: { $isArray: '$author.posts' }, then: { $size: '$author.posts' }, else: 0} },
+            'author.followersNumber': { $cond: { if: { $isArray: '$author.followers' }, then: { $size: '$author.followers' }, else: 0} },
+            'author.followingNumber': { $cond: { if: { $isArray: '$author.following' }, then: { $size: '$author.following' }, else: 0} },
+        }},
+        { $project: projectOptionsSavedPost },
+        { $group: {
+            '_id': '$_id',
+            'posts': { $push: '$posts' },
+        }},
+        { $sort: { 'date': -1 } }
+    ])
 
-    res.status(200).json(posts);
+    res.status(200).json(posts[0].posts);
 }
 
 const createPost = async (req, res) => {
@@ -128,7 +200,7 @@ const createPost = async (req, res) => {
     })
 
     const postCreated = await newPost.save();
-    await User.findByIdAndUpdate(user._id, { $push: { 'posts': postCreated._id }, $inc: { 'postsNumber': 1 } }, {safe: true, upsert: true, new : true});
+    await User.findByIdAndUpdate(user._id, { $push: { 'posts': postCreated._id } }, {safe: true, upsert: true, new : true});
     await Category.findByIdAndUpdate(post.category._id, { $push: { 'posts': postCreated._id } }, {safe: true, upsert: true, new : true});
 
     res.status(201).json(postCreated);
@@ -138,35 +210,81 @@ const likePost = async (req, res) => {
     const post = req.body;
     const user = req.user;
 
-    let postUpdated;
-
     if (post.likes.includes(user._id)) {
-        postUpdated = await Post.findByIdAndUpdate(post._id, { $pull: { 'likes': user._id } }, {safe: true, upsert: true, new : true});
-        await User.findByIdAndUpdate(user._id, { $pull: { 'likes': postUpdated._id } }, {safe: true, upsert: true, new : true});
+        await Post.findByIdAndUpdate(post._id, { $pull: { 'likes': user._id } }, {safe: true, upsert: true, new : true});
+        await User.findByIdAndUpdate(user._id, { $pull: { 'likes': post._id } }, {safe: true, upsert: true, new : true});
     } else {
-        postUpdated = await Post.findByIdAndUpdate(post._id, { $push: { 'likes': user._id }, $pull: { 'dislikes': user._id } }, {safe: true, upsert: true, new : true});
-        await User.findByIdAndUpdate(user._id, { $push: { 'likes': postUpdated._id }, $pull: { 'dislikes': postUpdated._id } }, {safe: true, upsert: true, new : true});
+        await Post.findByIdAndUpdate(post._id, { $push: { 'likes': user._id }, $pull: { 'dislikes': user._id } }, {safe: true, upsert: true, new : true});
+        await User.findByIdAndUpdate(user._id, { $push: { 'likes': post._id }, $pull: { 'dislikes': post._id } }, {safe: true, upsert: true, new : true});
     }
 
-    await postUpdated.populate([{ path: 'author', select: '-posts -followers -following -password'},{ path: 'category', select: '-posts' } ]);
-    res.status(201).json(postUpdated);
+    const postUpdated = await Post.aggregate([
+        { $match: { $expr: { $eq: ['$_id', { $toObjectId: post._id } ] } } },
+        { $lookup: {
+            from: 'users', 
+            localField: 'author', 
+            foreignField: '_id', 
+            as: 'author' 
+        }},
+        { $unwind: '$author' },
+        { $lookup: { 
+            from: 'categories', 
+            localField: 'category', 
+            foreignField: '_id', 
+            as: 'category' 
+        }},
+        { $unwind: '$category' },
+        { $addFields: {
+            'numReplies': { $cond: { if: { $isArray: '$replies' }, then: { $size: '$replies' }, else: 0} },
+            'author.postNumber': { $cond: { if: { $isArray: '$author.posts' }, then: { $size: '$author.posts' }, else: 0} },
+            'author.followersNumber': { $cond: { if: { $isArray: '$author.followers' }, then: { $size: '$author.followers' }, else: 0} },
+            'author.followingNumber': { $cond: { if: { $isArray: '$author.following' }, then: { $size: '$author.following' }, else: 0} },
+        }},
+        { $project: projectOptionsPost },
+        { $sort: { 'date': -1 } }
+    ])
+
+    res.status(201).json(postUpdated[0]);
 }
 
 const dislikePost = async (req, res) => {
     const post = req.body;
     const user = req.user;
 
-    let postUpdated;
-
     if (post.dislikes.includes(user._id)) {
-        postUpdated = await Post.findByIdAndUpdate(post._id, { $pull: { 'dislikes': user._id } }, {safe: true, upsert: true, new : true});
-        await User.findByIdAndUpdate(user._id, { $pull: { 'dislikes': postUpdated._id } }, {safe: true, upsert: true, new : true});
+        await Post.findByIdAndUpdate(post._id, { $pull: { 'dislikes': user._id } }, {safe: true, upsert: true, new : true});
+        await User.findByIdAndUpdate(user._id, { $pull: { 'dislikes': post._id } }, {safe: true, upsert: true, new : true});
     } else {
-        postUpdated = await Post.findByIdAndUpdate(post._id, { $push: { 'dislikes': user._id }, $pull: { 'likes': user._id } }, {safe: true, upsert: true, new : true});
-        await User.findByIdAndUpdate(user._id, { $push: { 'dislikes': postUpdated._id }, $pull: { 'likes': postUpdated._id } }, {safe: true, upsert: true, new : true});
+        await Post.findByIdAndUpdate(post._id, { $push: { 'dislikes': user._id }, $pull: { 'likes': user._id } }, {safe: true, upsert: true, new : true});
+        await User.findByIdAndUpdate(user._id, { $push: { 'dislikes': post._id }, $pull: { 'likes': post._id } }, {safe: true, upsert: true, new : true});
     }
 
-    await postUpdated.populate([{ path: 'author', select: '-posts -followers -following -password'},{ path: 'category', select: '-posts' } ]);
+    const postUpdated = await Post.aggregate([
+        { $match: { $expr: { $eq: ['$_id', { $toObjectId: post._id } ] } } },
+        { $lookup: {
+            from: 'users', 
+            localField: 'author', 
+            foreignField: '_id', 
+            as: 'author' 
+        }},
+        { $unwind: '$author' },
+        { $lookup: { 
+            from: 'categories', 
+            localField: 'category', 
+            foreignField: '_id', 
+            as: 'category' 
+        }},
+        { $unwind: '$category' },
+        { $addFields: {
+            'numReplies': { $cond: { if: { $isArray: '$replies' }, then: { $size: '$replies' }, else: 0} },
+            'author.postNumber': { $cond: { if: { $isArray: '$author.posts' }, then: { $size: '$author.posts' }, else: 0} },
+            'author.followersNumber': { $cond: { if: { $isArray: '$author.followers' }, then: { $size: '$author.followers' }, else: 0} },
+            'author.followingNumber': { $cond: { if: { $isArray: '$author.following' }, then: { $size: '$author.following' }, else: 0} },
+        }},
+        { $project: projectOptionsPost },
+        { $sort: { 'date': -1 } }
+    ])
+
     res.status(201).json(postUpdated);
 }
 
@@ -174,17 +292,40 @@ const savePost = async (req, res) => {
     const post = req.body;
     const user = req.user;
 
-    let postUpdated;
-
     if (post.saves.includes(user._id)) {
-        postUpdated = await Post.findByIdAndUpdate(post._id, { $pull: { 'saves': user._id } }, {safe: true, upsert: true, new : true});
-        await User.findByIdAndUpdate(user._id, { $pull: { 'saves': postUpdated._id } }, {safe: true, upsert: true, new : true});
+        await Post.findByIdAndUpdate(post._id, { $pull: { 'saves': user._id } }, {safe: true, upsert: true, new : true});
+        await User.findByIdAndUpdate(user._id, { $pull: { 'saves': post._id } }, {safe: true, upsert: true, new : true});
     } else {
-        postUpdated = await Post.findByIdAndUpdate(post._id, { $push: { 'saves': user._id } }, {safe: true, upsert: true, new : true});
-        await User.findByIdAndUpdate(user._id, { $push: { 'saves': postUpdated._id } }, {safe: true, upsert: true, new : true});
+        await Post.findByIdAndUpdate(post._id, { $push: { 'saves': user._id } }, {safe: true, upsert: true, new : true});
+        await User.findByIdAndUpdate(user._id, { $push: { 'saves': post._id } }, {safe: true, upsert: true, new : true});
     }
 
-    await postUpdated.populate([{ path: 'author', select: '-posts -followers -following -password'},{ path: 'category', select: '-posts' } ]);
+    const postUpdated = await Post.aggregate([
+        { $match: { $expr: { $eq: ['$_id', { $toObjectId: post._id } ] } } },
+        { $lookup: {
+            from: 'users', 
+            localField: 'author', 
+            foreignField: '_id', 
+            as: 'author' 
+        }},
+        { $unwind: '$author' },
+        { $lookup: { 
+            from: 'categories', 
+            localField: 'category', 
+            foreignField: '_id', 
+            as: 'category' 
+        }},
+        { $unwind: '$category' },
+        { $addFields: {
+            'numReplies': { $cond: { if: { $isArray: '$replies' }, then: { $size: '$replies' }, else: 0} },
+            'author.postNumber': { $cond: { if: { $isArray: '$author.posts' }, then: { $size: '$author.posts' }, else: 0} },
+            'author.followersNumber': { $cond: { if: { $isArray: '$author.followers' }, then: { $size: '$author.followers' }, else: 0} },
+            'author.followingNumber': { $cond: { if: { $isArray: '$author.following' }, then: { $size: '$author.following' }, else: 0} },
+        }},
+        { $project: projectOptionsPost },
+        { $sort: { 'date': -1 } }
+    ])
+
     res.status(201).json(postUpdated);
 }
 
